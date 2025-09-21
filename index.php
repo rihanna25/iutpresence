2434a5dd726b899d1879d95f7a410aa50f3b1972
Your Name <you@example.com>
2025-09-21 21:49:57 +0100

    premier push

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$IUT = [
+    'classe' => [
+        'audio' => [
+            'eleve1' => [
+                'nom' => 'BAHA',
+                'prenom' => 'ephraim',
+                'notes' => [20, 20, 10]
+            ],
+            'eleve2' => [
+                'nom' => 'ngako',
+                'prenom' => 'stephy',
+                'notes' => [10, 12, 10]
+
+            ],
+        ],
+        'complexe' => [
+            'eleve1' => [
+                'nom' => 'MOPI',
+                'prenom' => 'PAYOLE',
+                'notes' => [20, 20, 20]
+            ],
+            'eleve2' => [
+                'nom' => 'KEPKO',
+                'prenom' => 'SOSO',
+                'notes' => [12, 12, 20]
+
+            ],
+
+        ]
+    ]
+];
