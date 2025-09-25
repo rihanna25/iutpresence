b6d639bcb82d5c648675e20df171573782dce8f1
Your Name <you@example.com>
2025-09-25 17:41:37 +0100

    ajout de h

--- condition.php
+++ condition.php
@@ -1,10 +1,24 @@
-<?php
-$annee_naissance = (int)readline('entrez votre annee de naissance:  ');
-$annee_actuelle = ("2025");
-$age = $annee_actuelle - $annee_naissance;
-echo (" tu as $age ans");
-if (" $age >18 ") {
-    echo (" statut: majeur");
-} else {
-    echo ("statut: mineur");
-}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $annee_naissance = 2006;
+    $annee_actuelle = 2025;
+    $age = $annee_actuelle - $annee_naissance;
+    echo " <h1>tu as $age ans</h1> ";
+    if ($age >= 18) {
+        echo " <h1>statut: majeur</h1> ";
+    } else {
+        echo " <h1> statut: mineur</h1> ";
+    }
+    ?>
+</body>
+
+</html>
